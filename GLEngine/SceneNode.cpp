9b5ee072df7e2f5ac9b6e425b8b9ea8376acc3db
Ethan Moffatt <darker1300@gmail.com>
2017-05-03 17:39:47 +1000

    SceneNode update.

--- GLEngine/SceneNode.cpp
+++ GLEngine/SceneNode.cpp
@@ -3,22 +3,40 @@
 #include <glm/ext.hpp>
 
 SceneNode::SceneNode()
+	: m_parent(nullptr)
+	, m_children()
+	, m_local(1.0f)
 {
 }
-
 
 SceneNode::~SceneNode()
 {
 }
 
+SceneNode * SceneNode::CreateChild()
+{
+	SceneNode* child = new SceneNode();
+	m_children.push_back(child);
+	child->m_parent = (SceneNode*)this;
+	return child;
+}
+
+glm::mat4 SceneNode::World() const
+{
+	return glm::mat4();
+}
+
 void SceneNode::Translate(const glm::vec3 & _localUnits)
 {
+	m_local = glm::translate(m_local, _localUnits);
 }
 
 void SceneNode::Rotate(float _degrees, const glm::vec3 & _axis)
 {
+	m_local = glm::rotate(m_local, _degrees, _axis);
 }
 
 void SceneNode::Scale(const glm::vec3 & _multiplier)
 {
+	m_local = glm::scale(m_local, _multiplier);
 }
--- GLEngine/SceneNode.h
+++ GLEngine/SceneNode.h
@@ -14,13 +14,14 @@ protected:
 public:
 	~SceneNode();
 
-	SceneNode* CreateChild() const;
+	SceneNode* CreateChild();
 	glm::mat4 World() const;
 
 	void Translate(const glm::vec3& _localUnits);
 	void Rotate(float _degrees, const glm::vec3& _axis);
 	void Scale(const glm::vec3& _multiplier);
 	
+	SceneNode* m_parent;
 	std::vector<SceneNode*> m_children;
 	glm::mat4 m_local;
 };
